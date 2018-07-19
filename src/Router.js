efb090f0f651015699f25074472eb87040d9435b
TheKipmaster <fkipman@hotmail.com>
2018-07-19 12:38:07 -0300

    More refactoring to integrate scenes correctly

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router>
-      <Scene key='root'>
-        <Scene key='login' component={LoginForm} title='Please Login' />
+      <Scene key='root' hideNavBar>
+        <Scene key='auth'>
+          <Scene
+            key='login'
+            component={LoginForm}
+            title='Please Login'
+            initial
+          />
+        </Scene>
+        <Scene key='main'>
+          <Scene
+            key='employeeList'
+            component={EmployeeList}
+            title='List of Employees'
+            rightTitle="Add"
+            onRight={() => { console.log('right!'); }}
+          />
+        </Scene>
       </Scene>
     </Router>
   );
